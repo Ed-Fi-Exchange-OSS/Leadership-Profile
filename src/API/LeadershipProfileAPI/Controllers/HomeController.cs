d046831e922eac469fb8277bfa7610fc01a7ef8f
YogirajA <aradhyeyogi@gmail.com>
2020-11-17 15:03:07 -0600

    Added API project

---
+++ src/API/LeadershipProfileAPI/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LeadershipProfileAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
