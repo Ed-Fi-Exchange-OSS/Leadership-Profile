6fddbd832bc6aaff55a7de2257793f6befbf3464
allisono-hs <allison.ohanlon@headspring.com>
2020-11-23 09:34:21 -0700

    beginning on building out functionality with hooks of the directory table

---
+++ src/Web/src/components/TableCompononets/TableList.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Table, Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+import { RightPointingIcon } from '../Icons'
+
+class List extends Component {
+    render() {
+        return (
+            <div>
+                <Table striped className="directory-table">
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>District</th>
+                        <th>School</th>
+                        <th>Position</th>
+                        <th>Years</th>
+                        <th>Degree</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>                    
+                                <span class="dot"></span>
+                            </td>
+                            <td>446235</td>
+                            <td>Angel Rivera</td>
+                            <td>Mesquite District</td>
+                            <td>Mesquite High School</td>
+                            <td>Teacher</td>
+                            <td>5</td>
+                            <td>Ph.D.</td>
+                            <td><RightPointingIcon /></td>
+                        </tr>
+                    </tbody>
+                <tfoot>
+                <span>Showing 1-10 of 340 Users</span>
+                    <Pagination size="sm" aria-label="Page navigation example">
+                        <PaginationItem>
+                            <PaginationLink previous href="#" />
+                        </PaginationItem>
+                        <PaginationItem>
+                            <PaginationLink href="#">
+                            1
+                            </PaginationLink>
+                        </PaginationItem>
+                        <PaginationItem>
+                            <PaginationLink href="#">
+                            2
+                            </PaginationLink>
+                        </PaginationItem>
+                        <PaginationItem>
+                            <PaginationLink href="#">
+                            3
+                            </PaginationLink>
+                        </PaginationItem>
+                        <PaginationItem>
+                            <PaginationLink next href="#" />
+                        </PaginationItem>
+                    </Pagination>
+                </tfoot>
+                </Table>
+                </div>
+        );
+    }
+}
+
+function CreateTableList() {
+    
+}
+
+const List = () => (
+    <CreatePulseCheckTable />
+  );
+
+export default List;
