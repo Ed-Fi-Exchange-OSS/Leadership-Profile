c9dcf84ed64f923bfa1ed152a6fc42fa663be8c2
allisono-hs <allison.ohanlon@headspring.com>
2020-12-01 12:07:37 -0700

    list page formatting done

--- src/Web/src/components/DirectoryComponents/PaginationButtons.js
+++ src/Web/src/components/DirectoryComponents/PaginationButtons.js
@@ -3,18 +3,19 @@ import PropTypes from 'prop-types';
 import { ButtonGroup, Button } from 'reactstrap';
 
 const PaginationButtons = (props) => {
+  console.log(props);
   const { paging, setPage } = props;
   const { page, maxPages } = paging;
   const intPage = parseInt(page, 0);
   return (
-    <div>
-      {intPage > 1 ? <a outline color="primary" onClick={() => setPage(intPage - 1)}>{'<'}</a> : ''}
-      {intPage > 2 ? <a outline color="primary" onClick={() => setPage(intPage - 2)}>{intPage - 2}</a> : ''}
-      {intPage > 1 ? <a outline color="primary" onClick={() => setPage(intPage - 1)}>{intPage - 1}</a> : ''}
-      <a color="primary" active>{page}</a>
-      {intPage < maxPages ? <a outline color="primary" onClick={() => setPage(intPage + 1)}>{intPage + 1}</a> : '' }
-      {intPage < maxPages - 2 ? <a outline color="primary" onClick={() => setPage(intPage + 2)}>{intPage + 2}</a> : '' }
-      {intPage <= maxPages - 1 ? <a outline color="primary" onClick={() => setPage(intPage + 1)}>{'>'}</a> : '' }
+    <div className="pagination-button-div">
+      {intPage > 1 ? <a className="pagination-arrows" onClick={() => setPage(intPage - 1)}>{'<'}</a> : ''}
+      {intPage > 2 ? <a onClick={() => setPage(intPage - 2)}>{intPage - 2}</a> : ''}
+      {intPage > 1 ? <a onClick={() => setPage(intPage - 1)}>{intPage - 1}</a> : ''}
+      <a class="current-pagination-page">{page}</a>
+      {intPage < maxPages ? <a onClick={() => setPage(intPage + 1)}>{intPage + 1}</a> : '' }
+      {intPage < maxPages - 2 ? <a onClick={() => setPage(intPage + 2)}>{intPage + 2}</a> : '' }
+      {intPage <= maxPages - 1 ? <a className="pagination-arrows" onClick={() => setPage(intPage + 1)}>{'>'}</a> : '' }
     </div>
   );
 };
