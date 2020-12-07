d29edd001aaca183eb1ff8daa81dae3671d3424d
YogirajA <aradhyeyogi@gmail.com>
2020-12-07 10:53:01 -0600

    Using the public API instead of sandbox

--- src/API/LeadershipProfileAPI/Constants.cs
+++ src/API/LeadershipProfileAPI/Constants.cs
@@ -3,6 +3,6 @@
     public class Constants
     {
         public static string ODSApiClient = "ODS-API-Client";
-        public const string VersionUriFragment = "data/v3";
+        public const string VersionUriFragment = "v5.0.0/api/data/v3";
     }
 }
