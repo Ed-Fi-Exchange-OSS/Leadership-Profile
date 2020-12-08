af1f657a37d1a1c1bb34e114b10652be4c1606c7
allisono-hs <allison.ohanlon@headspring.com>
2020-12-08 13:26:31 -0800

    styling buttons for view
    sort has pointer cursor on hover
    
    Added StaffUniqueId for correct data associations, added a test for exception response and all tests pass
    
    made paging numbers appear more like links
    
    staff unique ID instead of guid
    
    centering view buttons
    
    Added legth check on array for address

--- src/Web/src/components/DirectoryComponents/Directory.js
+++ src/Web/src/components/DirectoryComponents/Directory.js
@@ -6,10 +6,11 @@ import DirectoryFilters from './DirectoryFilters';
 import TableList from './TableList';
 import CardList from './CardListComponents/CardList';
 import UseDirectory from './UseDirectory';
+import ErrorMessage from '../ErrorMessage';
 
 const Directory = () => {
     const [activeComponent, setActiveComponent] = useState("table");
-    const { setColumnSort, sort, data, paging, setPage, search, setSearchValue } = UseDirectory();
+    const { setColumnSort, sort, data, paging, setPage, search, setSearchValue, error } = UseDirectory();
 
     return (
         <div>
@@ -19,16 +20,17 @@ const Directory = () => {
                     <BreadcrumbList currentPage="home" />
                     <div className="view-style-buttons">
                         <span className="view-style-label">View Style</span>
-                        <Button color="primary" className="view-style-button-first" onClick={() => setActiveComponent("table")}>
+                        <Button color="primary" className="view-style-button-first view-style-button" onClick={() => setActiveComponent("table")}>
                             <TableViewIcon />
                         </Button>
-                        <Button color="primary" onClick={() => setActiveComponent("card")}>
+                        <Button color="primary" className="view-style-button" onClick={() => setActiveComponent("card")}>
                             <CardViewIcon />
                         </Button>
                     </div>
                 </div>
             </div>
             <DirectoryFilters search={search} setSearchValue={setSearchValue} />
+            { error ? <ErrorMessage /> : '' }
             { activeComponent === "table" ? (
                 <TableList sort={sort} data={data} setColumnSort={setColumnSort} paging={paging} setPage={setPage} />
             ) : activeComponent === "card" ? (
