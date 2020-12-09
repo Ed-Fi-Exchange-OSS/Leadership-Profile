9b35770af5783dd07411571eaff420ac76bf3cff
allisono-hs <allison.ohanlon@headspring.com>
2020-12-09 11:42:48 -0800

    styling for profile

--- src/Web/src/components/ProfileComponents/CollapsibleTable.js
+++ src/Web/src/components/ProfileComponents/CollapsibleTable.js
@@ -1,27 +1,44 @@
 import React, { useState } from 'react';
 import { Collapse, Table } from 'reactstrap';
+import { EducationIcon, BriefcaseIcon, ChartIcon, CertificateIcon, DownPointingIcon } from '../Icons';
+
+function getIcon(title) {
+    switch (title) {
+        case "Education":
+            return <EducationIcon />;
+        case "Position History":
+            return <BriefcaseIcon />;
+        case "Certifications":
+            return <CertificateIcon />;
+        case "Professional Development and Learning Experiences":
+            return <ChartIcon />;
+    }
+
+}
 
 const CollapsibleTable = (props) => {
+    const { title, categories } = props;
     const [isOpen, setIsOpen] = useState(false);
 
     const toggle = () => setIsOpen(!isOpen);
 
     return (
-        <div>
-            <h2 onClick={toggle}>Education</h2>
+        <div className="profile-collapsible-container">
+            <h2 className="profile-collapsible-header" onClick={toggle}>
+                <span className="profile-collapsible-icon">{getIcon(title)}</span>
+                <span>{title}</span>
+                <span className="profile-collapsible-down-icon"><DownPointingIcon /></span>
+            </h2>
             <Collapse isOpen={isOpen}>
-                <Table striped>
-                    <thead>
-                        <tr>
-                            <th>Institution</th>
-                            <th>Degree</th>
-                            <th>Date</th>
-                            <th>Specialization</th>
+                <Table striped className="profile-collapsible-table">
+                    <thead className="profile-table-head">
+                        <tr className="profile-table-header-row">
+                            {categories.map(category => (<th>{category}</th>))}
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th>University of North Texas</th>
+                        <tr className="profile-table-body-row">
+                            <td>University of North Texas</td>
                             <td>Doctorate</td>
                             <td>2004</td>
                             <td>Education Administration</td>
