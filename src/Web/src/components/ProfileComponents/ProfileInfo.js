68ee9095dea4dc6146cf5656af49fe118f653562
allisono-hs <allison.ohanlon@headspring.com>
2020-12-11 13:46:17 -0800

    mapping data to profile info

--- src/Web/src/components/ProfileComponents/ProfileInfo.js
+++ src/Web/src/components/ProfileComponents/ProfileInfo.js
@@ -2,42 +2,43 @@ import React from 'react';
 import { Row, Col, Card, CardTitle, CardText, Table } from 'reactstrap';
 import { PersonIcon, GeoIcon, PhoneIcon, MailIcon, EducationIconNavy, RibbonIcon, CalendarIcon, IdIcon } from '../Icons';
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    const { data } = props;
     return (
         <Card body>
             <Row>
                 <Col sm="1">
-                    <img width="88px" height="88px" src="https://hips.hearstapps.com/countryliving.cdnds.net/17/47/1511194376-cavachon-puppy-christmas.jpg" alt="profile" className="rounded-circle" />
+                    <img src="https://tvline.com/wp-content/uploads/2014/08/school-of-rock.jpg?w=300&h=208&crop=1" alt="profile" className="rounded-circle" />
                 </Col>
                 <Col>
                     <table>
                         <thead>
-                            <th colSpan="3"className="profile-card-title">Dr. Angel Rivera</th>
+                            <th colSpan="3"className="profile-card-title">{data.fullName}</th>
                         </thead>
                         <tbody>
                             <tr>
                                 <td className="profile-info-icon"><PersonIcon /></td>
-                                <td className="profile-info-text">Principal</td>
+                                <td className="profile-info-text">{data.currentPosition}</td>
                                 <td className="profile-info-icon"><GeoIcon /></td>
-                                <td className="profile-info-text">Mesquite District</td>
+                                <td className="profile-info-text">{data.district}</td>
                                 <td className="profile-info-icon"><PhoneIcon /></td>
-                                <td className="profile-info-text">+1 123 123 1233</td>
+                                <td className="profile-info-text">{data.phone}</td>
                             </tr>
                             <tr>
                                 <td className="profile-info-icon"><IdIcon /></td>
-                                <td className="profile-info-text">ID 12345</td>
+                                <td className="profile-info-text">ID {data.staffUniqueId}</td>
                                 <td className="profile-info-icon"><EducationIconNavy /> </td>
-                                <td className="profile-info-text">Mesquite High School</td>
+                                <td className="profile-info-text">{data.school}</td>
                                 <td className="profile-info-icon"><MailIcon /></td>
-                                <td className="profile-info-text">arivera@mesquite.edu</td>
+                                <td className="profile-info-text">{data.email}</td>
                             </tr>
                             <tr>
-                                <td className="profile-info-icon"><span className="green-dot" /></td>
-                                <td className="profile-info-text">Interested in next role</td>
+                                <td className="profile-info-icon">{data.interestedInNextRole ? <span className="green-dot small-dot" /> : <span className="red-dot small-dot" /> }</td>
+                                <td className="profile-info-text">{data.interestedInNextRole ? "Interested in next role" : "Not seeking the next role"}</td>
                                 <td className="profile-info-icon"><RibbonIcon /></td>
-                                <td className="profile-info-text">5 years of service</td>
+                                <td className="profile-info-text">{data.yearsOfService} years of service</td>
                                 <td className="profile-info-icon"><CalendarIcon /></td>
-                                <td className="profile-info-text">Last start date: 11/11/2015</td>
+                                <td className="profile-info-text">Last start date: {formatDate(data.startDate)}</td>
                             </tr>
                         </tbody>
                     </table>
@@ -46,5 +47,10 @@ const ProfileInfo = () => {
         </Card>
     );
 }
+
+function formatDate(dateString) {
+    var date = new Date(dateString);
+    return date.toLocaleDateString("en-US");
+};
 
 export default ProfileInfo;
