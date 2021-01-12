f27cc87c64b3a3866ea394de580339a2cd697f17
YogirajA <aradhyeyogi@gmail.com>
2021-01-12 16:55:29 -0600

    CORS url fix, login return value and response type

--- src/API/LeadershipProfileAPI/Controllers/ProfileController.cs
+++ src/API/LeadershipProfileAPI/Controllers/ProfileController.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using LeadershipProfileAPI.Infrastructure;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.WebUtilities;
