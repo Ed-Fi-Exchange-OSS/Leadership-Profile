1cf9c48252bdba9feb41eceeee88a06861a9f70e
YogirajA <aradhyeyogi@gmail.com>
2021-01-13 16:41:07 -0600

    Added Dbcontext for normal db operations and renamed identity dbcontext

---
+++ src/API/LeadershipProfileAPI/Data/EdFiDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LeadershipProfileAPI.Data
+{
+    public class EdFiDbContext : DbContext
+    {
+        public EdFiDbContext(DbContextOptions<EdFiDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Staff> Staff { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {   
+            modelBuilder.Entity<Staff>().ToTable("Staff", schema:"edfi")
+                .Property(p=>p.LastName).HasColumnName("LastSurname");
+        }
+    }
+}
