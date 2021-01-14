caf831da1a18cdea6ddcab3bd703e4ebc973192b
allisono-hs <allison.ohanlon@headspring.com>
2021-01-13 22:50:37 -0700

    need to correctly show error message and do redirects

--- src/Web/src/components/LoginComponents/UseRegistration.js
+++ src/Web/src/components/LoginComponents/UseRegistration.js
@@ -11,68 +11,88 @@ function UseRegistration() {
     const [registrationInfo, setRegistrationInfo] = useState({
         username: email,
         password: password,
-        email: email, 
+        email: email,
         staffUniqueId: staffUniqueId,
         firstName: firstName,
         lastName: lastName,
     });
-    const [formComplete, setFormComplete] = useState({
-        valid: false,
+    const [error, setError] = useState({
+        hasError: false,
         message: ''
     });
 
     useEffect(() => {
-        setRegistrationInfo({
-            username: email,
-            password: password,
-            email: email, 
-            staffUniqueId: staffUniqueId,
-            firstName: firstName,
-            lastName: lastName,
-        });
-        setFormComplete(validateForm());
+        if (password === confirmPassword) {
+            setRegistrationInfo({
+                username: email,
+                password: password,
+                email: email,
+                staffUniqueId: staffUniqueId,
+                firstName: firstName,
+                lastName: lastName,
+            });
+        } else {
+            setError({
+                hasError: true,
+                message: 'Passwords must match.'
+            });
+        }
     }, [email, password, staffUniqueId, firstName, lastName]);
 
     async function setRegistration() {
-        if (formComplete) {
-            let unmounted = false;
-            const apiUrl = new URL(`https://localhost:44383/account/register`);
-            Axios.post(apiUrl, registrationInfo).then((response) => {
-                if (!unmounted && response.data !== null) {
-                    setError(false);
-                    setData(response.data);
-                }
-            }).catch(error => {
-                setError(true);
-                console.error(error.message);
+        let unmounted = false;
+        const apiUrl = new URL(`https://localhost:44383/account/register`);
+        Axios.post(apiUrl, registrationInfo).then((response) => {
+            if (!unmounted && response.data !== null) {
+                setError(false);
+                console.log(response.data)
+            }
+        }).catch((error) => {
+            const errorResponse = error.response.data
+            console.log(Object.values(errorResponse.errors.join()));
+            setError({
+                hasError: errorResponse.isError,
+                message: Object.values(errorResponse.errors)
+                    .join(' ')
             });
-            return () => {
-                unmounted = true;
-            };
-        }
+            console.error(error.message);
+        });
+        return () => {
+            unmounted = true;
+        };
     }
 
-    function validateForm() {
-        let allFieldsComplete = registrationInfo.filter(field => field !== '');
-        if (allFieldsComplete.includes(false)) {
-            return {
-                valid: false,
-                message: 'All fields are required.'
+    return {
+        setRegistration,
+        error,
+        bind: {
+            registrationInfo,
+            onChange: event => {
+                switch (event.target.name) {
+                    case 'email':
+                        setEmail(event.target.value);
+                        break;
+                    case 'password':
+                        setPassword(event.target.value);
+                        break;
+                    case 'confirmPassword':
+                        setConfirmPassword(event.target.value);
+                        break;
+                    case 'staffUniqueId':
+                        setStaffUniqueId(event.target.value);
+                        break;
+                    case 'firstName':
+                        setFirstName(event.target.value);
+                        break;
+                    case 'lastName':
+                        setLastName(event.target.value);
+                        break;
+                    default:
+                        console.error('No matching elements');
+                }
             }
-        }
-
-        let passwordsMatch = password === confirmPassword;
-        if (!passwordsMatch) {
-            return {
-                valid: false,
-                message: 'Password required with matching confirmation.'
-            }
-        }
-
-        return allFieldsComplete && passwordsMatch;
+        },
     }
-
-    return {setRegistration, formComplete}
 }
 
-UseRegistration
+export default UseRegistration
