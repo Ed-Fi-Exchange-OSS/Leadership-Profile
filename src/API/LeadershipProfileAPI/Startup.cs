1aad593c5a425977c23ce71f17905b37eab15cd6
Javier Gonzalez <javier.gonzalez@headspring.com>
2021-01-19 13:06:52 -0600

    Add EmailSender

--- src/API/LeadershipProfileAPI/Startup.cs
+++ src/API/LeadershipProfileAPI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Net.Http;
 using LeadershipProfileAPI.Infrastructure.Auth;
+using LeadershipProfileAPI.Infrastructure.Email;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -26,6 +27,8 @@ namespace LeadershipProfileAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<EmailSettings>(Configuration.GetSection("EmailSettings"));
+
             services.AddCors(); // Make sure you call this previous to AddMvc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
 
@@ -36,6 +39,8 @@ namespace LeadershipProfileAPI
                     x => { x.BaseAddress = new Uri(Configuration["ODS-API"]); })
                 .SetHandlerLifetime(TimeSpan.FromMinutes(handlerLifeTimeInMinutes))
                 .AddHttpMessageHandler<AuthenticationDelegatingHandler>();
+
+            services.AddTransient<IEmailSender, SmtpSender>();
 
             services.AddControllers();
 
