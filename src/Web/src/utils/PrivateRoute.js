e72622ad2ff3d671b598659fb804f991ecfcdf47
allisono-hs <allison.ohanlon@headspring.com>
2021-01-21 15:13:39 -0700

    login redirect works and data appearing

--- src/Web/src/utils/PrivateRoute.js
+++ src/Web/src/utils/PrivateRoute.js
@@ -4,7 +4,6 @@ import Login from '../components/LoginComponents/Login';
 
 const PrivateRoute = ({ isAuthenticated, component, ...options }) => {
   const finalComponent = isAuthenticated ? component : Login;
-  console.log(options);
 
   return <Route {...options} component={finalComponent} />;
 };
