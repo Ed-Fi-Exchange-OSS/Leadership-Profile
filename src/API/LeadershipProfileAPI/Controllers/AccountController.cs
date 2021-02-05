23da0627b1487dfef15797f684ad93d6e8f753f3
javier1martinez <77988781+javier1martinez@users.noreply.github.com>
2021-02-05 14:22:15 -0600

    Revert "Revert "Forgot password feature""
    This reverts commit 3666d87620384f78737ccd664ef25547d432b67b.

--- src/API/LeadershipProfileAPI/Controllers/AccountController.cs
+++ src/API/LeadershipProfileAPI/Controllers/AccountController.cs
@@ -15,6 +15,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+using LeadershipProfileAPI.Infrastructure.Email;
+using System.Collections.Generic;
+using System;
 
 namespace LeadershipProfileAPI.Controllers
 {   
@@ -28,6 +31,7 @@ namespace LeadershipProfileAPI.Controllers
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly EdFiDbContext _dbContext;
+        private readonly IEmailSender _emailSender;
 
         public AccountController(
             IIdentityServerInteractionService interaction,
@@ -36,14 +40,51 @@ namespace LeadershipProfileAPI.Controllers
             IEventService events,
             SignInManager<IdentityUser> signInManager,
             UserManager<IdentityUser> userManager,
-            EdFiDbContext dbContext)
+            EdFiDbContext dbContext,
+            IEmailSender emailSender)
         {
             _events = events;
             _signInManager = signInManager;
             _userManager = userManager;
             _dbContext = dbContext;
+            _emailSender = emailSender;
         }
 
+        [HttpPost("forgotPassword")]
+        public async Task<ForgotPasswordResultModel> ForgotPasswordAsync(ForgotPasswordModel model)
+        {
+            // Gets user by username from Staff table
+            var staff = _dbContext.Staff.SingleOrDefault(s => s.TpdmUsername == model.Username && s.StaffUniqueId == model.StaffUniqueId);
+
+            if (staff == null)
+                throw new ApiExceptionFilter.ApiException("Staff record not found.");
+
+            // Get user by username from ASP.Net table
+            var user = await _signInManager.UserManager.FindByNameAsync(model.Username);
+            
+            // Generate token and reset link
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            
+            var resetPasswordQueryString = new Dictionary<string, string> { { "token", token }, { "username", model.Username } };
+            var callback = new Uri(Microsoft.AspNetCore.WebUtilities.QueryHelpers.AddQueryString("http://" + Request.Host.Host + "/Account/ResetPassword", resetPasswordQueryString)).ToString();
+
+            var message = $"<h2>Click the link below to reset your password.</h2><br/><br/><p>{callback}</p>";
+            await _emailSender.SendEmailAsync(user.Email, "Reset Password", message);
+
+            return new ForgotPasswordResultModel { Result = true, ResultMessage = "An email will be sent to the email address on file in the system." };
+        }
+
+        [HttpPost("resetPassword")]
+        public async Task<ResetPasswordResultModel> ResetPasswordAsync(ResetPasswordModel model)
+        {
+            var user = await _signInManager.UserManager.FindByNameAsync(model.Username);
+
+            var result = await _userManager.ResetPasswordAsync(user, model.Token, model.NewPassword);
+
+            return result.Succeeded
+                ? new ResetPasswordResultModel { Result = true, ResultMessage = "Password changed." }
+                : new ResetPasswordResultModel { Result = false, ResultMessage = "Reset password failed." };
+        }
         [HttpPost("register")]
         public async Task<LoginResultModel> Register(RegisterModel model)
         {
@@ -169,6 +210,34 @@ namespace LeadershipProfileAPI.Controllers
         }
     }
 
+    public class ForgotPasswordModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string StaffUniqueId { get; set; }
+    }
+
+    public class ForgotPasswordResultModel
+    {
+        public bool Result { get; set; }
+        public string ResultMessage { get; set; }
+    }
+
+    public class ResetPasswordModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string NewPassword { get; set; }
+        [Required]
+        public string Token { get; set; }
+    }
+    public class ResetPasswordResultModel
+    {
+        public bool Result { get; set; }
+        public string ResultMessage { get; set; }
+    }
     public class RegisterModel  
     {
         [Required]
