ebe8d622557c381c7485bd15df1e5876bfff69e3
javier1martinez <javier.martinez@headspring.com>
2021-02-24 16:32:04 -0600

    Refactor Collapsible
    Refactor Collapsible to PositionHistory component

---
+++ src/Web/src/components/PositionHistoryComponent/PositionHistoryTable.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import { CardTitle, Collapse, Table } from 'reactstrap';
+import { BriefcaseIcon, DownPointingIcon } from '../Icons';
+
+const positionHistoryCategories = { 'role': 'Role', 'schoolName': 'School', 'startDate': 'Start Date', 'endDate': 'End Date' };
+
+const PositionHistoryTable = (props) => {
+    const { title, data } = props;
+    const [isOpen, setIsOpen] = useState(false);
+    const [icon, setIcon] = useState('');
+    const [categories, setCategories] = useState({});
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    function setTable(title) {
+        setIcon(<BriefcaseIcon />);
+        setCategories(positionHistoryCategories);
+    }
+    useEffect(() => {
+        setTable(title);
+    }, [CardTitle]);
+
+    return (
+        <div className="profile-collapsible-container">
+            <h2 className="profile-collapsible-header" onClick={toggle}>
+                <span className="profile-collapsible-icon">{icon}</span>
+                <span>{title}</span>
+                <span className="profile-collapsible-down-icon"><DownPointingIcon /></span>
+            </h2>
+            <Collapse isOpen={isOpen}>
+                <Table striped className="profile-collapsible-table">
+                    <thead className="profile-table-head">
+                        <tr className="profile-table-header-row">
+                            {Object.values(categories).map(category => (<th key={category}>{category}</th>))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data !== undefined ? data.map(row =>
+                        (<tr className="profile-table-body-row">
+                            {Object.keys(categories).map(value => {
+                                let displayValue = row[`${value}`];
+                                displayValue = (value.toLowerCase()).includes('date') ? formatDate(displayValue) : displayValue;
+                                return (<td>{displayValue}</td>)
+                            })}
+                        </tr>)) : ''
+                        }
+                    </tbody>
+                </Table>
+            </Collapse>
+        </div>
+    );
+}
+
+function formatDate(dateString) {
+    var date = new Date(dateString);
+    return date.toLocaleDateString("en-US");
+};
+
+export default PositionHistoryTable;
