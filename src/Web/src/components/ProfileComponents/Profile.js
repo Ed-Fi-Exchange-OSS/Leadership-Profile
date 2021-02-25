86fb1eff698ce6ba97386d8aa1d2d92839019304
javier1martinez <javier.martinez@headspring.com>
2021-02-25 12:17:32 -0600

    Refactor Professional Development
    Refactor Professional Development Collapsible.
    Professional Development has it's own Component.

--- src/Web/src/components/ProfileComponents/Profile.js
+++ src/Web/src/components/ProfileComponents/Profile.js
@@ -4,6 +4,7 @@ import { Nav, NavItem, NavLink} from 'reactstrap';
 import ProfileInfo from './ProfileInfo';
 import CollapsibleTable from './CollapsibleTable';
 import LeaderOfOrgChart from './LeaderOfOrgChart';
+import ProfessionalDevelopmentTable from './../ProfessionalDevelopmentComponents/ProfessionalDevelopmentTable'
 import UseProfile from './UseProfile';
 
 const Profile = () => {
@@ -28,7 +29,7 @@ const Profile = () => {
                     <CollapsibleTable title='Education' data={data.education} />
                     <CollapsibleTable title='Position History' data={data.positionHistory} />
                     <CollapsibleTable title='Certifications' data={data.certificates} />
-                    <CollapsibleTable title='Professional Development and Learning Experiences' data={data.professionalDevelopment}/>
+                    <ProfessionalDevelopmentTable title='Professional Development and Learning Experiences' data={data.professionalDevelopment}/>
                 </div>
                 ) : activeComponent === "leader" ? (
                     <LeaderOfOrgChart />
