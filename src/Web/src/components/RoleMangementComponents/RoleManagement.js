d125ad4567ef810bc52072b3f361ca26b13a2e16
javier1martinez <javier.martinez@headspring.com>
2021-03-03 12:54:15 -0600

    Change Role Management layout
    Added Breadcrumb
    Added Header Card
    Table check box centered

--- src/Web/src/components/RoleMangementComponents/RoleManagement.js
+++ src/Web/src/components/RoleMangementComponents/RoleManagement.js
@@ -2,48 +2,63 @@ import React from 'react';
 import { Table, Button, Form, FormGroup, Input, Alert } from 'reactstrap';
 import PaginationButtons from '../DirectoryComponents/PaginationButtons';
 import UseRoleManagement from './UseRoleManagement';
+import BreadcrumbList from '../Breadcrumb';
 
 const RoleManagement = () => {
     const { data, paging, SetPage, OnSubmit, error, bind } = UseRoleManagement();
 
     return (
         <div>
+            <div className='directory-div'>
+                <h2 className='directory-title role-card-title'>Role Managment</h2>
+                <div className="directory-subtitle-controls">
+                    <BreadcrumbList currentPage="home" />
+                </div>
+            </div>
             {error ?
                 <Alert color="danger">
                     Unable to load data.
                 </Alert>
                 : ''}
             <Form>
-                <Table>
-                    <tr>
-                        <th>Admin</th>
-                        <th>Staff ID</th>
-                        <th>Name</th>
-                        <th>Username</th>
-                        <th>Location</th>
-                    </tr>
-                    {data.profiles !== undefined && data.profiles !== [] ? data.profiles.map(profile => (
-                        <tr key={profile.id}>
-                            <td>
-                                <FormGroup>
-                                    <Input type="checkbox" defaultChecked={profile.isAdmin} 
-                                    value={profile.staffUniqueId} {...bind} />
-                                </FormGroup>
-                            </td>
-                            <td>{profile.staffUniqueId}</td>
-                            <td>{profile.lastSurname}, {profile.firstName}</td>
-                            <td>{profile.username}</td>
-                            <td>{profile.location}</td>
+                <Button size="lg" onClick={event => OnSubmit(event)}>Save</Button>
+                <Table striped className="directory-table">
+                    <thead>
+                        <tr>
+                            <th>Admin</th>
+                            <th>User</th>
+                            <th>Staff ID</th>
+                            <th>Name</th>
+                            <th>Username</th>
                         </tr>
-                    )) : ''}
-                    <tr className="bottom-row">
-                        <td colSpan="4">
-                            <Button onClick={event => OnSubmit(event)}>Save</Button>
-                        </td>
-                        <td colSpan="1" className="pagination-buttons-container">
-                            <PaginationButtons paging={paging} setPage={SetPage} />
-                        </td>
-                    </tr>
+                    </thead>
+                    <tbody>                        
+                        {data.profiles !== undefined && data.profiles !== [] ? data.profiles.map(profile => (
+                            <tr key={profile.id}>
+                                <td>
+                                    <FormGroup className="isdamin-checkbox">
+                                        <Input type="checkbox" defaultChecked={profile.isAdmin} 
+                                        value={profile.staffUniqueId} {...bind} />
+                                    </FormGroup>
+                                </td>
+                                <td>{profile.staffUniqueId}</td>
+                                <td>{profile.lastSurname}, {profile.firstName}</td>
+                                <td>{profile.username}</td>
+                                <td>{profile.location}</td>
+                            </tr>
+                        )) : ''}
+                        <tr className="bottom-row">
+                            {/* <td colSpan="4">
+                                <Button onClick={event => OnSubmit(event)}>Save</Button>
+                            </td> */}
+                            <td colSpan="4">
+                                <span>Showing {paging.page}-10 of {paging.totalSize} Users</span>
+                            </td>
+                            <td colSpan="1" className="pagination-buttons-container">
+                                <PaginationButtons paging={paging} setPage={SetPage} />
+                            </td>
+                        </tr>
+                    </tbody>
                 </Table>
             </Form>
         </div>
