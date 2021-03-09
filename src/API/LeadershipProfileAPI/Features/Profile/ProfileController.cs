cd648060f71fcd5c5ee84ed7addb2db042621cad
javier1martinez <javier.martinez@headspring.com>
2021-03-09 10:04:29 -0600

    Added modal component to profile
    Added modal component to profile
    Added endpoint to API

--- src/API/LeadershipProfileAPI/Features/Profile/ProfileController.cs
+++ src/API/LeadershipProfileAPI/Features/Profile/ProfileController.cs
@@ -1,4 +1,5 @@
-﻿using LeadershipProfileAPI.Infrastructure;
+﻿using LeadershipProfileAPI.Data.Models;
+using LeadershipProfileAPI.Infrastructure;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -41,6 +42,26 @@ namespace LeadershipProfileAPI.Features.Profile
         public async Task<ActionResult> GetProfile([FromRoute] string id, CancellationToken cancellationToken)
         {
             var result = await _mediator.Send(new Get.Query {Id = id}, cancellationToken);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost("datacorrection")]
+        public async Task<ActionResult> SendDataCorrection(DataCorrectionModel data, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(new DataCorrection.Command
+            {
+                MessageDescription = data.MessageDescription,
+                MessageSubject = data.MessageSubject,
+                StaffEmail = data.StaffEmail,
+                StaffUniqueId = data.StaffUniqueId,
+                UserFullName = data.UserFullName
+            });
 
             if (result == null)
             {
--- src/Web/src/components/ProfileComponents/ProfileInfo.js
+++ src/Web/src/components/ProfileComponents/ProfileInfo.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import { Row, Col, Card, CardTitle, CardText, Table } from 'reactstrap';
+import React, { useState } from 'react';
+import { Row, Col,  Button, Card, Modal, ModalHeader, ModalBody, ModalFooter, Label, Input, Form, FormGroup } from 'reactstrap';
 import { PersonIcon, GeoIcon, PhoneIcon, MailIcon, EducationIconNavy, RibbonIcon, CalendarIcon, IdIcon } from '../Icons';
+import UseFeedbackModal from './UseFeedbackModal';
 
 const ProfileInfo = (props) => {
     const { data } = props;
+    const {sendFeedback, bind} = UseFeedbackModal();
+    
+    const [modal, setModal] = useState(false);
+    const toggle = () => setModal(!modal);
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        const data = {"sui": e.currentTarget[0].value,
+            "fullname": e.currentTarget[1].value,
+            "email": e.currentTarget[2].value}
+        sendFeedback(data);
+
+        setModal(false);
+    };
+
     return (
         <Card body>
             <Row>
@@ -42,8 +58,51 @@ const ProfileInfo = (props) => {
                                 <td className="profile-info-icon"><CalendarIcon /></td>
                                 <td className="profile-info-text">Last start date: {formatDate(data.startDate)}</td>
                             </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td><Button color="primary" onClick={toggle}>Request Data Correction</Button></td>
+                            </tr>
                         </tbody>
                     </table>
+                    <Modal isOpen={modal} toggle={toggle} fullscreen="sm">
+                        <ModalHeader toggle={toggle}>Feedback</ModalHeader>
+                        <ModalBody>
+                            <Form id="feedback-form" onSubmit={e => handleOnSubmit(e)}>
+                                <Input type="hidden" name="staffUniqueId" id="staffUniqueId" value={data.staffUniqueId}></Input>
+                                <Input type="hidden" name="fullName" id="fullName" value={data.fullName}></Input>
+                                <Input type="hidden" name="email" id="email" value={data.email}></Input>
+
+                                <div className="mb-2 mr-sm-2 mb-sm-0">
+                                    <Label for="lblsubject" className="label-feedback mr-sm-2">StaffUniqueId: </Label>
+                                    <Label for="lblsubject" className="mr-sm-2">{data.staffUniqueId}</Label>
+                                </div>
+                                <div className="mb-2 mr-sm-2 mb-sm-0">
+                                    <Label for="staffsubject" className="label-feedback mr-sm-2">Staff Name: </Label>
+                                    <Label for="staffsubject" className="mr-sm-2">{data.fullName}</Label>
+                                </div>
+                                <div className="mb-2 mr-sm-2 mb-sm-0">
+                                    <Label for="emailsubject" className="label-feedback mr-sm-2">Staff Email: </Label>
+                                    <Label for="emailsubject" className="mr-sm-2">{data.email}</Label>
+                                </div>
+                                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                                    <Label for="subject" className="label-feedback mr-sm-2">Subject</Label>
+                                    <Input type="text" name="subject" id="subject" {...bind}/>
+                                </FormGroup>
+                                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                                    <Label for="messagescontent" className="label-feedback mr-sm-2">Description</Label>
+                                    <Input type="textarea" name="messagescontent" id="messagecontent"  {...bind}/>
+                                </FormGroup>
+                            </Form>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color="primary" form="feedback-form">Submit</Button>{' '}
+                            <Button color="secondary" onClick={toggle}>Cancel</Button>
+                        </ModalFooter>
+                    </Modal>
                 </Col>
             </Row>
         </Card>
