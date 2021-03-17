2875ccb23089417e9600e5501b31b3444a7e5cb0
javier1martinez <javier.martinez@headspring.com>
2021-03-17 14:52:30 -0600

    CardProfile Responsive
    Card profile responsive
    added classes to Crad profile

--- src/Web/src/components/DirectoryComponents/CardListComponents/CardList.js
+++ src/Web/src/components/DirectoryComponents/CardListComponents/CardList.js
@@ -6,33 +6,19 @@ import CardProfile from './CardProfile';
 const CardList = (props) => {
     
     const {data} = props;
-    const dataTable = [];
-
-    for(var i = 0; i <= data.length; i = i+3)
-    {
-        dataTable.push(data.splice(i, i+3));
-    }
-
     return (
         <div>
-            {dataTable.map((e,i) => {
-                return(
-                    <div>
-                        <br></br>
-                        <Row key={i}>
-                            {                        
-                                e.map((ee, ii) => {
-                                    return (
-                                    <Col sm="4" key={ii}>
-                                        <CardProfile data={ee}></CardProfile>
-                                    </Col>
-                                    );
-                                })
-                            }
-                        </Row>
-                    </div>
-                );
-            })}
+            {
+                <Row key="1" className="col-md-12">
+                    { 
+                    data.map((profile, index) =>{
+                        return(
+                            <CardProfile data={profile}></CardProfile>
+                        );
+                    })
+                }
+                </Row>               
+            }
         </div>
     );
 }
--- src/Web/src/components/DirectoryComponents/CardListComponents/CardProfile.js
+++ src/Web/src/components/DirectoryComponents/CardListComponents/CardProfile.js
@@ -8,29 +8,31 @@ const CardProfile = (props) => {
     const {data} = props;
 
     return(
-        <Card>
-            <Row sm="3">
-                <Col sm="2">
-                    <DefaultProfile></DefaultProfile>
-                </Col>
-                <Col sm="9">
-                    <CardTitle tag="h5">{data.fullName}</CardTitle>
-                    <CardText><MailIcon />{data.email}</CardText>
-                    <CardText><PhoneIcon />{data.telePhone}</CardText>
-                </Col>
-                <Col sm="1">
-                    <RightPointingIcon />
-                </Col>
-            </Row>
-            <Row>
-                <Col sm={{ size:3, offset:2}}>
-                    <CardText><PersonIcon />{data.highestDegree}</CardText>
-                </Col>
-                <Col>
-                    <CardText><GeoIcon />{data.location}</CardText>
-                </Col>
-            </Row>
-        </Card>
+        <div className="col-md-4 col-sm-4 card-spacing">
+            <Card>
+                <Row sm="3">
+                    <Col sm="2">
+                        <DefaultProfile></DefaultProfile>
+                    </Col>
+                    <Col sm="9">
+                        <CardTitle tag="h5">{data.fullName}</CardTitle>
+                        <CardText><MailIcon />{data.email}</CardText>
+                        <CardText><PhoneIcon />{data.telePhone}</CardText>
+                    </Col>
+                    <Col sm="1">
+                        <RightPointingIcon />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={{ size:3, offset:2}}>
+                        <CardText><PersonIcon />{data.highestDegree}</CardText>
+                    </Col>
+                    <Col>
+                        <CardText><GeoIcon />{data.location}</CardText>
+                    </Col>
+                </Row>
+            </Card>
+        </div>
     )
 }
 
