5a5ee743cf06bf49693f526e4dc2cbecdc62e8d5
Patrick McVeety-Mill <patrickm@headspring.com>
2021-09-02 18:16:50 -0500

    add constructor for years range

--- src/API/LeadershipProfileAPI/Data/Models/ProfileSearchRequest/ProfileSearchYearsOfPriorExperience.cs
+++ src/API/LeadershipProfileAPI/Data/Models/ProfileSearchRequest/ProfileSearchYearsOfPriorExperience.cs
@@ -8,6 +8,14 @@ namespace LeadershipProfileAPI.Data.Models.ProfileSearchRequest
 
         public class Range
         {
+            public Range() { }
+
+            public Range(int min, int max)
+            {
+                Min = min;
+                Max = max;
+            }
+
             public int Min { get; set; }
             public int Max { get; set; }
         }
