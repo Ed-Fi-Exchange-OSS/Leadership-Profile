eeb2cf58f6eee80e426b661fb201c9c439a12cdd
Patrick McVeety-Mill <patrickm@headspring.com>
2021-09-02 16:54:42 -0500

    nest class to show intent

--- src/API/LeadershipProfileAPI/Data/Models/ProfileSearchRequest/ProfileSearchRequestTenure.cs
+++ src/API/LeadershipProfileAPI/Data/Models/ProfileSearchRequest/ProfileSearchRequestTenure.cs
@@ -5,11 +5,11 @@ namespace LeadershipProfileAPI.Data.Models.ProfileSearchRequest
     public class ProfileSearchYearsOfPriorExperience
     {
         public ICollection<Range> Values { get; set; }
-    }
 
-    public class Range
-    {
-        public int Min { get; set; }
-        public int Max { get; set; }
+        public class Range
+        {
+            public int Min { get; set; }
+            public int Max { get; set; }
+        }
     }
 }
